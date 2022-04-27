3f46456b07e62f22b17a3b27a79e97e87d95c782
Khajornsak Chaipha <khajornsakchaipha@Khajornsaks-MacBook-Pro.local>
2022-04-28 03:28:00 +0700

    update project2022/04/28

--- src/app/pages/login-register/login-register.component.ts
+++ src/app/pages/login-register/login-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{Route, Router}from '@angular/router';
 
 @Component({
   selector: 'app-login-register',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-register.component.scss']
 })
 export class LoginRegisterComponent implements OnInit {
+  //ตัวแปร ธรรมดา
+  message:string ="1"
 
-  constructor() { }
+  //ตัวแปร Object
+profile = {
+  "name":"Samit",
+  "tel":"99999999",
+  "gender":"Male"
+}
+
+  //Tow way data binding To Object
+  userData = {
+    "email":"",
+    "password":"",
+  }
+    // ดึงมาจาก Router
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+  // Fc Login SubmitLogin
+  submitLogin(){
+   //alert("Hey")
+   if(this.userData.email == "admin@gmail.com"&&this.userData.password == "1234")
+   {
+     //alert("Login Success");
+     this.router.navigate(['backend']);
+   }else{
+     alert("Login Faill!!!");
+   }
   }
 
 }
